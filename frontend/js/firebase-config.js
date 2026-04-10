296a6de3d58409f1ee1303212856d3e3cb4c368b
Dhanenthira M <dhanenthira2005@example.com>
2026-04-10 15:52:32 +0530

    change

--- frontend/js/firebase-config.js
+++ frontend/js/firebase-config.js
@@ -2,11 +2,11 @@
 // Replace these with your actual Firebase project config
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: FIREBASE_AUTH_DOMAIN,
-  projectId: FIREBASE_PROJECT_ID,
-  storageBucket:FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
-  appId: FIREBASE_APP_ID
+  authDomain:Process.env.FIREBASE_AUTH_DOMAIN,
+  projectId:Process.env.FIREBASE_PROJECT_ID,
+  storageBucket:Process.env. FIREBASE_STORAGE_BUCKET,
+  messagingSenderId:Process.env. FIREBASE_MESSAGING_SENDER_ID,
+  appId:Process.env. FIREBASE_APP_ID
 };
 
 // Initialize Firebase
